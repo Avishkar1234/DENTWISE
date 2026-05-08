972e90b08384ae6f7ed2ed56a98909168668ee02
Avishkar <avishkar1234567890@gmail.com>
2026-05-08 20:38:53 +0530

    appointments page added

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatar.iran.liara.run",
-        pathname: "/**",
-      },
       {
         protocol: "https",
         hostname: "api.dicebear.com",
